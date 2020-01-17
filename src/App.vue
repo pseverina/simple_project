aa852354b781ee3f36b2e47de3be706896b44639
PSeverina <severich@yandex.ru>
2020-01-17 19:58:50 +0300

    created and setup the work environment

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  <div id="app">
+    <Select/>
+  </div>
+</template>
+
+<script>
+import Select from './components/Select'
+
+export default {
+  name: 'App',
+  components: {
+    Select
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>

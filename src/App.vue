b9fb8d0bc2a78d132928f0da790f8faebda0becb
PSeverina <severich@yandex.ru>
2020-01-20 20:34:50 +0300

    fixed select component, added search option

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
-  #app
-    Select
+  <div id="app">
+    <Select :options="icecreams"></Select>
+  </div>
 </template>
 
 <script>
@@ -10,6 +11,33 @@ export default {
   name: 'App',
   components: {
     Select
+  },
+  data () {
+    return {
+      chosen_item: '',
+      drinks: [
+        {
+          name: 'Кока-кола',
+          id: 1
+        },
+        {
+          name: 'Апельсиновый сок',
+          id: 2
+        },
+        {
+          name: 'Водичка',
+          id: 3
+        }
+      ],
+      food: {
+        option1: 'Чипсоны',
+        option2: 'Борщ',
+        option3: 'Доширак',
+        option4: 'Оливье',
+        option5: 'Холодец'
+      },
+      icecreams: ['ванильное мороженое', 'шоколадное мороженое', 'сливочное мороженое']
+    }
   }
 }
 </script>
--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,44 +1,37 @@
 <template lang="pug">
-  .container
-    input.select(list='order', onmouseover='focus(); old = value', onmousedown="value = ''", onmouseup='value = old', placeholder='Выберите продукт')
-    datalist#order(v-model='chosen_item')
-      option(v-for='drink in drinks', :key='drink.id', :value='drink.name') {{ drink.name }}
-      optgroup(label='Еда')
-        option(v-for='option in food', :key='option') {{ option }}
-      optgroup(label='Десерт')
-        option(v-for='icecream in icecreams', :key='icecream') {{ icecream }}
-      option(value=' ', readonly='') Вино
+  <div class="custom-select" @blur="open = false">
+    <input type="text" v-model="search" class="select" :class="{open: open}" @click="open = !open" :placeholder="`${selected}`"/>
+    <div class="items" :class="{selectHide: !open}">
+      <div class="item" v-for="(option, index) in filteredList" :key="index" @click="selected=option; open=false; $emit('input', option)">{{ option }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Select',
+  props: {
+    options: {
+      type: Array,
+      required: true
+    }
+  },
   data () {
     return {
-      chosen_item: '',
-      drinks: [
-        {
-          name: 'Кока-кола',
-          id: 1
-        },
-        {
-          name: 'Апельсиновый сок',
-          id: 2
-        },
-        {
-          name: 'Водичка',
-          id: 3
-        }
-      ],
-      food: {
-        option1: 'Чипсоны',
-        option2: 'Борщ',
-        option3: 'Доширак',
-        option4: 'Оливье',
-        option5: 'Холодец'
-      },
-      icecreams: ['Ванильное мороженое', 'Шоколадное мороженое', 'Сливочное мороженое']
+      search: '',
+      selected: this.options.length > 0 ? this.options[0] : null,
+      open: false
     }
+  },
+  computed: {
+    filteredList () {
+      return this.options.filter(el => {
+        return el.match(this.search)
+      })
+    }
+  },
+  mounted () {
+    this.$emit('input', this.selected)
   }
 }
 </script>
